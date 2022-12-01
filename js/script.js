7dca7b651ec880eb37420c42e5c0d639c1fc1101
SaraDonzellini <saradonzie@gmail.com>
2022-12-01 12:56:20 +0100

    getting  16 random numbers

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 const buttonPlay = document.querySelector(".btn");
-
+let bombsArray = [];
 
 buttonPlay.addEventListener('click', function () {
 
     const divBoxElement = document.querySelector(".box");
     divBoxElement.innerHTML = "";
     divBoxElement.classList.add('my_border');
-  
+    let newSquare;
+
     for (let i = 0; i < 100; i++) {
-      const newSquare = getNewSquare();
+      newSquare = getNewSquare();
       divBoxElement.appendChild(newSquare);
       newSquare.append(i + 1);
     }
 
+    for (let i = 0; i < 16; i++) {
+      bombsArray.push(getRandomUniqueNumber(bombsArray, 1, 100));
+    }
+
+    
 
 })
 
-
+console.log(bombsArray)
 
 function getNewSquare() {
   const newSquare = document.createElement("div");
@@ -27,3 +33,28 @@ function getNewSquare() {
   })
   return newSquare;
 }
+
+
+function getRandomUniqueNumber(blackList, min, max){
+  let isValid = false;
+  let randomNum;
+
+  while (isValid === false) {
+    randomNum = getRandomNumber(min,max);
+
+    if (!blackList.includes(randomNum)){
+      isValid = true;
+    }
+  }
+
+  return randomNum;
+}
+
+
+function getRandomNumber (min, max){
+  const numeroRandomico = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return numeroRandomico;
+}
+
+
